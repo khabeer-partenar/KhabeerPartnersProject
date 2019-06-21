96300850398269213bc062e5afa5b771980cec85
Abdullah Alqudiry <a.alqudiry@mu.edu.sa>
2019-06-21 15:39:01 +0300

    changes

---
+++ Modules/Users/Traits/SessionFlash.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Mamdouh Magdy <mamdouh95@mu.edu.sa>
+ */
+
+namespace Modules\Users\Traits;
+
+use Illuminate\Support\Facades\Session;
+
+trait SessionFlash {
+
+    public function sessionSuccess($msg = 'general.done successfaully')
+    {
+        return session::flash('alert-success', __("users::$msg"));
+    }
+}
