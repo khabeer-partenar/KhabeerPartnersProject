a2428e0fd80a1ed4a84cdc7f6c8e4e0b1f1d814e
Abdullah Alqudiry <a.alqudiry@mu.edu.sa>
2019-07-07 04:14:24 +0300

    changes

--- Modules/Users/Resources/views/coordinators/scripts.blade.php
+++ Modules/Users/Resources/views/coordinators/scripts.blade.php
@@ -1,39 +1,7 @@
 <script>
     $(document).ready(function () {
         $('.select2').select2();
-        // Load Department
-        $('.load-departments').change(function () {
-            let path = $(this).attr('data-url') + '?parentId=' + $(this).val();
-            let child = $(this).attr('data-child');
-            // Empty Children
-            $(child).empty();
-            $('#department_reference').val('');
-            $('#department_reference_val').val('');
-            if ($(child).hasClass('load-departments')) {
-                let childOfChild = $(child).attr('data-child');
-                $(childOfChild).empty();
-            }
-            if ($(this).val() != 0){
-                $.ajax({
-                    url: path,
-                    success: function (response) {
-                        let select = $(child)[0];
-                        let length = Object.keys(response).length;
-                        for (let index = 0; index < length; index++) {
-                            select.options[select.options.length] = new Option(response[index].name, response[index].id);
-                        }
-                        let children = $(select).children();
-                        for(let index = 0; index < length; index++) {
-                            if (response[index].reference_department) {
-                                $(children[index]).attr('data-ref-id', response[index].reference_department.id);
-                                $(children[index]).attr('data-ref-name', response[index].reference_department.name);
-                            }
-                            $(children[index]).attr('data-is-ref', response[index].is_reference);
-                        }
-                    }
-                });
-            }
-        });
+
         // Change Reference input
         $(document).on('change', '.change-reference', function () {
             let selectedOption = $(this).find(":selected")[0],
