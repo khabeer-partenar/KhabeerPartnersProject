bc9a2941751b94bfc94f12b3a29b7e8ca47654f0
HassanYoussefDev <38058471+HassanYoussefDev@users.noreply.github.com>
2019-07-25 04:58:44 +0300

    changes

--- Modules/Users/Http/Controllers/CoordinatorController.php
+++ Modules/Users/Http/Controllers/CoordinatorController.php
@@ -89,7 +89,7 @@ class CoordinatorController extends UserBaseController
      */
     public function store(SaveCoordinatorRequest $request)
     {
-        return $request;
+       // return $request;
         $coordinator = Coordinator::createFromRequest($request);
         $coordinator->log('create_coordinator');
         self::sessionSuccess('users::coordinators.created');
