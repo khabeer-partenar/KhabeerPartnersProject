752ed2dcb8ee224c4ef87e8ed50d673bb443aa09
ahmed desoky <ahmeddesoky@ahmeds-MacBook-Pro.local>
2020-01-16 13:04:47 +0300

    fix some issues on authorized names print blade

--- Modules/Committee/Http/Controllers/CommitteeAuthorizedNameController.php
+++ Modules/Committee/Http/Controllers/CommitteeAuthorizedNameController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Response;
 use Modules\Committee\Entities\AuthorizedName;
 use Modules\Core\Entities\Group;
 use Modules\Core\Traits\Log;
-use Modules\Users\Traits\SessionFlash;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\AuthorizedListExport;
 use niklasravnsborg\LaravelPdf\Facades\Pdf;
@@ -23,32 +22,32 @@ class CommitteeAuthorizedNameController extends UserBaseController
      * @param Request $request
      * @return Response
      */
-    public function index(Request $request, AuthorizedName $authorizedNames)
+    public function index(Request $request)
     {
 
         $lists = AuthorizedName::search($request->all())->paginate(10);
         $types = AuthorizedName::TYPE_TEXT;
         $advisors = Group::advisorUsersFilter()->filterByJob()->pluck('users.name', 'users.id');
-        return view('committee::authorizedName.authorized_list', compact('lists', 'advisors', 'types'));  
+        return view('committee::authorizedName.authorized_list', compact('lists', 'advisors', 'types'));
 
     }
 
     /**
-     * @return \Illuminate\Support\Collection
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function export(Request $request, AuthorizedName $authorizedNames) 
+    public function export(Request $request)
     {
         return Excel::download(new AuthorizedListExport, 'list.xlsx');
-        
+
     }
 
-    public function printAuthorizedList(AuthorizedName $authorizedNames, Request $request)
+    public function printAuthorizedList(Request $request)
     {
         $lists = AuthorizedName::search($request->all())->get();
-        $pdf = PDF::loadView('committee::authorizedName.print', compact('lists')); 
+        $pdf = PDF::loadView('committee::authorizedName.print', compact('lists'));
         return $pdf->stream('document.pdf');
 
     }
 
-   
+
 }
--- Modules/Committee/Resources/views/authorizedName/print.blade.php
+++ Modules/Committee/Resources/views/authorizedName/print.blade.php
@@ -2,12 +2,54 @@
 <html lang="{{ app()->getLocale() }}" dir="rtl">
 
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @include('layouts.report.authorized_list_report')
 
     {{--    <img src="https://www.google.pl/images/srpr/logo11w.png"/>--}}
+    <style type="text/css">
+    
+      
+
+        .table-striped tbody tr:nth-of-type(odd) {
+            padding: 90px;
+             border: 1px solid black;
+
+            border-collapse: collapse;
+
+            background-color: rgba(0,0,0,.015);
+        }
+
+        .table tbody td {
+            border-collapse: collapse;
+
+            border: 1px solid black;
+            border-bottom: 1px solid #ff5722;
+            font-size: 16px;
+        }
+
+        .table thead th {
+             border: 1px solid black;
+
+            border-bottom: 1px solid #ff5722;
+        }
+
+        .text-warning {
+            color: #ff5722 !important;
+        }
+
+        .tag {
+            padding: 1em 1em;
+        }
+        
+        
+        
+
+        
+    </style>
 
 </head>
 <div class="portlet light bordered">
@@ -22,7 +64,7 @@
         <br><br>
         <br>
         <br>
-        <table class="table">
+        <table id="table " class="table table-bordered table-responsive">
 
            
                     <thead>
