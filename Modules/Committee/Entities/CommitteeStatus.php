dd61e3f3a19ab9e3d79e9cc72edda8a7923304cc
Mamdouh <MamdouhMagdy95@gmail.com>
2020-06-15 18:47:48 +0300

    Fix Committee Status Bug

--- Modules/Committee/Entities/CommitteeStatus.php
+++ Modules/Committee/Entities/CommitteeStatus.php
@@ -36,28 +36,16 @@ class CommitteeStatus extends Model
 
     public static function updateCommitteeGroupsStatusToNominationsCompleted(Committee $committee,$status)
     {
-        $secretaryGroupId = Group::where('key', Employee::SECRETARY)->first()->id;
-        $advisorGroupId = Group::where('key', Employee::ADVISOR)->first()->id;
-        $officeOfThePresidentGroupId = Group::where('key', Employee::OFFICE_OF_THE_PRESIDENT)->first()->id;
-        $directorOfConsultantsOfficesGroupId = Group::where('key', Employee::DIRECTOR_OF_CONSULTANTS_OFFICES)->first()->id;
-        $portfolioManagerGroupId = Group::where('key', Employee::PORTFOLIO_MANAGER)->first()->id;
-        $technicalSupportGroupId = Group::where('key', Employee::TECHNICAL_SUPPORT)->first()->id;
-        $chairmanOfTheCommissionGroupId = Group::where('key', Employee::CHAIRMAN_OF_THE_COMMISSION)->first()->id;
-        $viceChairmanOfTheCommissionGroupId = Group::where('key', Employee::VICE_CHAIRMAN_OF_THE_COMMISSION)->first()->id;
-        $delegateGroupId = Group::where('key', Delegate::JOB)->first()->id;
+        $employeeGroups = Group::where('key', '<>', [
+            Coordinator::MAIN_CO_JOB,
+            Coordinator::NORMAL_CO_JOB
+        ])->pluck('id');
 
-        $committee->groupsStatuses()->syncWithoutDetaching(
-            [$secretaryGroupId => ['status' => $status]
-                , $advisorGroupId => ['status' => $status]
-                , $officeOfThePresidentGroupId => ['status' => $status]
-                , $directorOfConsultantsOfficesGroupId => ['status' => $status]
-                , $portfolioManagerGroupId => ['status' => $status]
-                , $technicalSupportGroupId => ['status' => $status]
-                , $chairmanOfTheCommissionGroupId => ['status' => $status]
-                , $viceChairmanOfTheCommissionGroupId => ['status' => $status]
-                , $delegateGroupId => ['status' => $status]
-            ]);
+        $syncArr = [];
+        foreach ($employeeGroups as $groupId) {
+            $syncArr[$groupId] = ['status' => $status];
+        }
 
+        $committee->groupsStatuses()->syncWithoutDetaching($syncArr);
     }
-
 }
